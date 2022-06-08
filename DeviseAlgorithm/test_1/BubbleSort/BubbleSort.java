d96193af542ca006f196a7d192c627084ad5269a
UserWangyin <1966307866@qq.com>
2022-06-08 06:59:59 +0000

    Message

---
+++ DeviseAlgorithm/test_1/BubbleSort/BubbleSort.java
@@ -0,0 +1,54 @@
+package DeviseAlgorithm.test_1.BubbleSort;
+
+//冒泡排序java实现
+import java.util.Scanner;
+public class BubbleSort{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("请输入数组长度：");
+            int n=input.nextInt();
+            int []arr=RandomArray(n, 10000);
+            System.out.println("排序前的数组：");
+            for (int i = 0; i < arr.length; i++) {
+            	System.out.print(arr[i]+" ");
+            }
+            BubbleSortAlgorithm(arr);
+            // System.out.println("\n排序后的数组：");
+            // for (int i = 0; i < arr.length; i++) {
+            // 	System.out.print(arr[i]+" ");
+            // }
+        }
+	}
+		public static int[]  RandomArray(int len,int max){
+			int[] arr=new int[len];
+			for(int i=0;i<arr.length;i++){
+				arr[i]=(int)(Math.random()*max);
+			}
+			return arr;
+	}
+    public static void BubbleSortAlgorithm(int[] arr){
+        //int temp;
+        int sum=0;
+        for (int i = 1; i < arr.length; i++) {
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+            sum=sum+arr.length-i;
+        }
+         System.out.println("\n排序后的数组：");
+        for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+    }
+    System.out.println("\n排序次数："+sum);
+    }
+}
